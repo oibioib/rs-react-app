08b6204b1044ab223d283a0101c22700472ae112
oibioib <pavelbodal@gmail.com>
2025-01-29 22:52:04 +0100

    feat: setup vite-tsconfig-paths

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
+import { Test } from '@components';
 import './App.css';
 
 const App = () => {
-  return <h1 className="bg-amber-400 text-4xl font-bold">RS React APP</h1>;
+  return <Test />;
 };
 
 export default App;
