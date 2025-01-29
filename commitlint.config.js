121888a71c11c0c988a627d68077f7eba63a3738
oibioib <pavelbodal@gmail.com>
2025-01-29 22:27:25 +0100

    feat: setup commitlint

---
+++ commitlint.config.js
@@ -0,0 +1,10 @@
+export default {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      ['init', 'feat', 'fix', 'refactor', 'docs', 'style', 'chore'],
+    ],
+  },
+};
